39d88d9c6b5e9841e7b945483db45b297aec6296
Тимурчишка и семья <kub-tima@yandex.ru>
2020-01-23 21:11:47 +0300

    11

--- vovels7.py
+++ vovels7.py
@@ -2,3 +2,4 @@ def search4letters(phrase: str, letters: str) -> set:
     """ sdhsjkdvndskjv111"""
     return set(letters).intersection(set(phrase))
      
+print (search4letters("Zdorovo mister", "retzob"))
