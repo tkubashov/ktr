70f2fd80cce271fbbef06c95003129d50395076a
Тимурчишка и семья <kub-tima@yandex.ru>
2020-01-23 21:32:46 +0300

    testing

--- vovels7.py
+++ vovels7.py
@@ -2,4 +2,4 @@ def search4letters(phrase: str, letters: str) -> set:
     """ sdhsjkdvndskjv111"""
     return set(letters).intersection(set(phrase))
      
-print (search4letters("Zdorovo mister", "retzob"))
+print (search4letters("Zdorovo mister", "master"))
