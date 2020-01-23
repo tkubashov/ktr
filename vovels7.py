e9909df8e56d98c804f07b8d341a5e96064ac02b
Тимурчишка и семья <kub-tima@yandex.ru>
2020-01-23 21:25:24 +0300

    testing

--- vovels7.py
+++ vovels7.py
@@ -3,3 +3,4 @@ def search4letters(phrase: str, letters: str) -> set:
     return set(letters).intersection(set(phrase))
      
 print (search4letters("Zdorovo mister", "retzob"))
+print ("Chekin git and pycharm")
