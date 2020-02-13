8c8404edeb6d31b8a1e1c741b315e9e71d517569
Тимурчишка и семья <kub-tima@yandex.ru>
2020-02-13 20:29:01 +0300

    testing

---
+++ WebApp.py
@@ -0,0 +1,3 @@
+from vovels7 import search4letters
+
+print(search4letters("Zdorovo mister", "ov"))
--- vovels7.py
+++
@@ -1,8 +0,0 @@
-def search4letters(phrase: str, letters: str) -> set:
-    """ sdhsjkdvndskjv111"""
-    return set(letters).intersection(set(phrase))
-     
-print (search4letters("Zdorovo mister", "retzob"))
-print ("Chekin git and pycharm")
-print (search4letters("Zdorovo mister", "retzob"))
-print ("Chekin git and pycharm")
