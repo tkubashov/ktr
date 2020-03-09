1d4a23210f028cf68b8f42adc2250f56c3506e56
Тимурчишка и семья <kub-tima@yandex.ru>
2020-03-09 14:51:56 +0300

    add debug and search4

--- WebApp.py
+++ WebApp.py
@@ -3,14 +3,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/search4', methods=['POST'])
 def do_search() -> str:
-    return str(search4letters('It is so cool', 'aeiyo'))
+    return str(search4letters('It is so cool', 'aeiyos'))
+
 
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html',
-                           the_title = 'Welcome to search4letters on the web')
+                           the_title='Welcome to search4letters on the web')
 
 
-app.run()
+app.run(debug=True)
