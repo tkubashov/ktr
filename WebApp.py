b9f56dd22325dc80daa949b1ec3996e2e6e636cb
Тимурчишка и семья <kub-tima@yandex.ru>
2020-03-09 16:03:16 +0300

    add debug and search4

--- WebApp.py
+++ WebApp.py
@@ -1,12 +1,26 @@
 from vovels7 import search4letters
-from flask import Flask, render_template
+from flask import Flask, render_template, request, escape
 
 app = Flask(__name__)
 
+def log_request(req: 'flask_request', res: str) -> None:
+    with open('vsearch.log', 'a') as log:
+        print(req.form, req.remote_addr, req.user_agent,res, file=log, sep=' | ')
+
+
 
 @app.route('/search4', methods=['POST'])
-def do_search() -> str:
-    return str(search4letters('It is so cool', 'aeiyos'))
+def do_search() -> 'html':
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    title = 'Here are your results'
+    results = str(search4letters(phrase, letters))
+    log_request(request, results)
+    return render_template('results.html',
+                           the_phrase = phrase,
+                           the_letters = letters,
+                           the_title = title,
+                           the_results = results,)
 
 
 @app.route('/entry')
@@ -14,5 +28,11 @@ def entry_page() -> 'html':
     return render_template('entry.html',
                            the_title='Welcome to search4letters on the web')
 
+
+@app.route('/viewlog')
+def view_the_log() -> str:
+    with open('vsearch.log') as log:
+        contents = log.read()
+    return escape(contents)
 
 app.run(debug=True)
