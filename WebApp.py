aa84210fa97cea056f18f196f9a086dd1fd74a6b
Тимурчишка и семья <kub-tima@yandex.ru>
2020-03-09 13:44:47 +0300

    testing

--- WebApp.py
+++ WebApp.py
@@ -1,11 +1,16 @@
 from vovels7 import search4letters
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def do_search() -> str:
     return str(search4letters('It is so cool', 'aeiyo'))
+
+@app.route('/entry')
+def entry_page() -> 'html':
+    return render_template('entry.html',
+                           the_title = 'Welcome to search4letters on the web')
 
 
 app.run()
